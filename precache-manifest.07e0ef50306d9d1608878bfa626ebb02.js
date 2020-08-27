ff240d7c6afc2ce872b5d1df35892bf1a91747aa
bharathjinka09 <bharathjinka09@gmail.com>
2020-08-27 13:42:11 +0530

    Updates

---
+++ precache-manifest.07e0ef50306d9d1608878bfa626ebb02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2766a763a94aa49757c62802ca01329c",
+    "url": "/react-bootstrap-easy-integration/index.html"
+  },
+  {
+    "revision": "1bf112d487ffc67428dd",
+    "url": "/react-bootstrap-easy-integration/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a6fb2abb50a23e384fe",
+    "url": "/react-bootstrap-easy-integration/static/js/2.f83d0f7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bootstrap-easy-integration/static/js/2.f83d0f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf112d487ffc67428dd",
+    "url": "/react-bootstrap-easy-integration/static/js/main.58709f5d.chunk.js"
+  },
+  {
+    "revision": "82dd505b9fa05f346aae",
+    "url": "/react-bootstrap-easy-integration/static/js/runtime-main.b9b2ed06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-bootstrap-easy-integration/static/media/logo.5d5d9eef.svg"
+  }
+]);
